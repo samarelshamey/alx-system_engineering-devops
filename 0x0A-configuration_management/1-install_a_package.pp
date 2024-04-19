3642426a4dac271a4aefbcebb3aa00569ee485ad
root <smsmelshamey90@gmail.com>
2024-04-19 16:35:46 -0700

    all

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # A puppet manifest installing flask from pip3.
 # specifying the version of flask 
 package { 'flask':
-ensure   => 2.1.0,
-provider => pip3,
+ensure   => '2.1.0',
+provider => 'pip3',
 }
