e0353c2be109d89788e3ed3027c4489e394a8699
root <smsmelshamey90@gmail.com>
2024-04-19 16:10:45 -0700

    all

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 # install package flask from pip3
-exec { 'installflask':
-command  => '/usr/bin/pip3 install flask==2.1.0',
-provider => 'shell',
-returns  => [0, 1],
+package { 'flask':
+ensure   => '2.1.0',
+provider => 'pip3',
 }
