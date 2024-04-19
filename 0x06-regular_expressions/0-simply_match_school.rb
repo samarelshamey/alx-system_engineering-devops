f52d3d9aea49502415c56b4f77d86bea95109ae2
root <smsmelshamey90@gmail.com>
2024-04-19 15:47:54 -0700

    all

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+#accept arg and define regular expression
+
+puts ARGV[0].scan(/School/).join
---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,6 @@
+#  install flask from pip3
+exec { 'installflask':
+command  => '/usr/bin/pip3 install flask==2.1.0',
+provider => 'shell',
+returns  => [0, 1],
+}
