e84889461070eb7593fb364037c847e49e22a068
root <smsmelshamey90@gmail.com>
2024-04-20 15:58:00 -0700

    ssh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#using puppet to make changes to our configuration file.
+file { '/etc/ssh/ssh_config':
+ensure => 'present',
+}
+file_line { 'disable pass auth':
+path  => '/etc/ssh/ssh_config',
+line  => 'PasswordAuthentication no',
+match => 'PasswordAuthentication',
+}
+file_line { 'declare idenity file':
+path  => '/etc/ssh/ssh_config',
+line  => 'IdentityFile ~/.ssh/school',
+match => 'IdentityFile',
+}
