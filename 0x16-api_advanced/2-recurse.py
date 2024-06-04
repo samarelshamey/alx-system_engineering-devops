71903eb66b533009ad3e69b5773869b6682f0748
root <smsmelshamey90@gmail.com>
2024-06-04 14:40:07 -0700

    api advance

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""eturns a list containing the titles of all hot articles for a given subreddit"""
+import requests
+
+
+def recurse(subreddit, hot_list=None, after=None):
+    if hot_list is None:
+        hot_list = []
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    params = {'limit': 100, 'after': after}
+    headers = {'User-Agent': 'Custom User Agent'}
+
+    try:
+        response = requests.get(url, params=params, headers=headers, allow_redirects=False)
+        response.raise_for_status()
+        data = response.json()
+        if 'data' in data and 'children' in data['data']:
+            children = data['data']['children']
+            for post in children:
+                hot_list.append(post['data']['title'])
+
+            after = data['data']['after']
+            if after:
+                return recurse(subreddit, hot_list, after)
+            else:
+                return hot_list
+        else:
+            return None
+    except requests.RequestException as e:
+        return None
